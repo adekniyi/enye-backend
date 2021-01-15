fab333e8e585a100927ac16dfd6a77588a6e0db3
Adek Niyi <adekniyi@gmail.com>
2021-01-14 19:48:54 -0800

    add Procfile to project

--- index.js
+++ index.js
@@ -12,12 +12,7 @@ app.get('/api/rates',(req,res)=>{
     var toRe =fetch(`https://api.exchangeratesapi.io/latest?base=${req.query.base}&symbols=${req.query.currency.split(",")[0]},${req.query.currency.split(",")[1]},${req.query.currency.split(",")[2]}`)
     .then((resp) => resp.json())
     .then((data) => {
-        if(data!=null||data!=undefined){
         res.json({result:{data}});
-        }
-        else{
-            res.status(400).json({message:"invalid query format pls make sure your url is like this '/api/rates?base=CZK&currency=EUR,GBP,USD'"})
-        }
     }).catch((err) => console.log(err));
   
     var toRes = {
